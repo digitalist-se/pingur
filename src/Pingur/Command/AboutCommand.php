18aefdf495def7579e526c383027694ca107dc2c
Mikke Schirén <mikke.schiren@digitalistgroup.com>
2017-11-22 14:43:16 +0100

    barebone

---
+++ src/Pingur/Command/AboutCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Pingur\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Process\Process;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+
+class AboutCommand extends Command
+{
+    protected $container;
+
+    public function __construct(ContainerBuilder $container)
+    {
+        parent::__construct();
+        $this->container = $container;
+    }
+
+    protected function configure()
+    {
+        $HelpText = '<info>Display the about</info>';
+
+        $this->setName("about")
+            ->setDescription("about pingur")
+            ->setHelp($HelpText);
+    }
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln("<info>pingur does pings" .
+        "</info>");
+    }
+}
---
+++ src/Pingur/Command/PingCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Pingur\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Process\Process;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Console\Input\Input;
+use Symfony\Component\Console\Input\InputOption;
+use JJG\Ping;
+
+
+class PingCommand extends Command {
+
+  protected $container;
+
+  public function __construct(ContainerBuilder $container) {
+    parent::__construct();
+    $this->container = $container;
+  }
+
+  protected function configure() {
+    $HelpText = 'The <info>ping</info> will ping url.
+<comment>Samples:</comment>
+<info>pingur ping --url=foobar.com</info>';
+
+    $this->setName("ping")
+      ->setDescription("ping a site")
+      ->setDefinition(
+        [
+          new InputOption(
+            'url',
+            'u',
+            InputOption::VALUE_OPTIONAL,
+            'URL to ping',
+            null
+          ),
+        ]
+      )
+      ->setHelp($HelpText);
+
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output) {
+
+    $host = $input->getOption('url');
+    $ping = new Ping($host);
+    $latency = $ping->ping();
+    if ($latency !== false) {
+      print 'Latency is ' . $latency . ' ms';
+    }
+    else {
+      print 'Host could not be reached.';
+    }
+  }
+}
